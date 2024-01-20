db6ceeec1a8b35b5873e7b5aaaf78e3f3b5d0ca3
flyan123 <2495365901@qq.com>
2024-01-20 23:29:36 +0800

    add part2

--- src/pages/Part/part2.jsx
+++ src/pages/Part/part2.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import part2style from './part2.module.less'
+
+import { Input, Space } from 'antd';
+const { Search } = Input;
+
+const onSearch = (value, _e, info) => console.log(info?.source, value);
 
 const Part2 = () => {
-    return(
-        <div>
-            这里是part2的内容
+    return (
+        <div className={part2style.all}>
+            <div className="searchbox">
+                
+                    <Space direction="vertical">
+                        <Search
+                            placeholder="请输入账号进行查询"
+                            onSearch={onSearch}
+                            style={{
+                                width: 500,
+                            }}
+                        />
+                    </Space>
+                    <button className="searchbut">添加用户</button>
+                
+
+            </div>
         </div>
     )
 }
